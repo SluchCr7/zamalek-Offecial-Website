eada0b68491ef30dfe80b15662e4c4d5a1a42ed1
Sluch <ahmedking10710@gmail.com>
2025-08-16 03:01:29 +0300

    new commit in pages

--- front/Components/ui/carousel.js
+++ front/Components/ui/carousel.js
@@ -92,7 +92,7 @@ const Slide = ({ slide, index, current, handleSlideClick }) => {
 const CarouselControl = ({ type, title, handleClick }) => {
   return (
     <button
-      className={`w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200`}
+      className="w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200"
       title={title}
       onClick={handleClick}
     >
@@ -128,7 +128,7 @@ export function Carousel({ slides }) {
 
   return (
     <div
-      className="relative w-[70vmin] h-[70vmin] mx-auto"
+      className="relative py-10 w-[300px] sm:w-[400px] md:w-[500px] lg:w-[600px] mx-auto"
       aria-labelledby={`carousel-heading-${id}`}
     >
       <ul
@@ -147,7 +147,9 @@ export function Carousel({ slides }) {
           />
         ))}
       </ul>
-      <div className="absolute flex justify-center w-full top-[calc(100%+1rem)]">
+
+      {/* أزرار التحكم */}
+      <div className="absolute inset-0 flex items-center justify-between px-2">
         <CarouselControl
           type="previous"
           title="Go to previous slide"
@@ -160,5 +162,6 @@ export function Carousel({ slides }) {
         />
       </div>
     </div>
+
   );
 }

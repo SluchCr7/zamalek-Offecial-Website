24074ae017c2224578b7be67cdd76358eb8d7335
Sluch <ahmedking10710@gmail.com>
2025-10-22 20:31:02 +0300

    Commit v38

--- front/Components/LayoutComponent.jsx
+++ front/Components/LayoutComponent.jsx
@@ -9,7 +9,7 @@ const LayoutComponent = ({ children }) => {
     const [opneModalAll , setOpenModalAll] = useState(false)
   return (
     <div>
-        <div className={`flex flex-col items-center gap-3 w-full`}>
+        <div className={`flex flex-col items-center w-full`}>
         
             <Nav opneModalAll={opneModalAll} setOpenModalAll={setOpenModalAll}/>
             {children}
